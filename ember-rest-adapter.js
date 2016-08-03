caeec862c12808219093f42123062abea68dc142
indr <mail@indr.ch>
2016-08-03 21:59:14 +0700

    Unit test initialization

--- ember-rest-adapter.js
+++ ember-rest-adapter.js
@@ -4,58 +4,62 @@ const _ = require('lodash');
 
 const plugin = 'ember-rest-adapter';
 
+const default_options = {
+
+};
+
 module.exports = function (options) {
   const seneca = this;
 
-  // options = options || {};
+  options = seneca.util.deepextend(default_options, options);
+
   // options.alias = options.alias || [];
   // options.alias['contact'] = ['contacts', 'foos'];
 
+  seneca.add({init: plugin}, init_plugin);
 
-  seneca.add({init: plugin}, seneca.util.nil);
+  function init_plugin(args, done) {
+    console.log('Initializing plugin ' + plugin);
+    const seneca = this;
 
-  // function init_plugin(args, done) {
-  //   console.log('Initializing plugin ' + plugin);
-  //   const seneca = this;
-  //
-  //   for (const to in options.alias) {
-  //     if (!options.alias.hasOwnProperty(to))
-  //       continue;
-  //
-  //     const from = _.isArray(options.alias[to]) ? options.alias[to] : [options.alias[to]];
-  //
-  //     from.forEach(function (each) {
-  //       _addAlias(seneca, each, to);
-  //     });
-  //   }
-  //
-  //
-  //   seneca.ready(function (err) {
-  //     if (err) return;
-  //
-  //     const pattern = 'role:jsonrest-api,method:*';
-  //     console.log('Adding wrapper ' + pattern);
-  //     var res = seneca.wrap(pattern, function (args, done) {
-  //       const seneca = this;
-  //
-  //       console.log('Intercepted ' + args.method + ' ' + args.prefix + '/' + args.kind);
-  //
-  //       if (args.method !== 'get') {
-  //         console.log('Normalizing ' + args.method + ' for name ' + args.name + ' / kind ' + args.kind);
-  //
-  //         if (args.data && Object.keys(args.data).length === 1 && args.data.hasOwnProperty(args.name)) {
-  //           console.log('Unwrapping root object ' + args.name);
-  //           args.data = args.data[args.name]
-  //         }
-  //       }
-  //
-  //       seneca.prior(args, _serialize(done, args.kind));
-  //     });
-  //   });
-  //
-  //   done(null, null);
-  // }
-  //
+    for (const to in options.alias) {
+      if (!options.alias.hasOwnProperty(to))
+        continue;
+
+      const from = _.isArray(options.alias[to]) ? options.alias[to] : [options.alias[to]];
+
+      from.forEach(function (each) {
+        _addAlias(seneca, each, to);
+      });
+    }
+
+
+    seneca.ready(function (err) {
+      if (err) return;
+
+      const pattern = 'role:jsonrest-api,method:*';
+      console.log('Adding wrapper ' + pattern);
+      var res = seneca.wrap(pattern, function (args, done) {
+        const seneca = this;
+
+        console.log('Intercepted ' + args.method + ' ' + args.prefix + '/' + args.kind);
+
+        if (args.method !== 'get') {
+          console.log('Normalizing ' + args.method + ' for name ' + args.name + ' / kind ' + args.kind);
+
+          if (args.data && Object.keys(args.data).length === 1 && args.data.hasOwnProperty(args.name)) {
+            console.log('Unwrapping root object ' + args.name);
+            args.data = args.data[args.name]
+          }
+        }
+
+        seneca.prior(args, _serialize(done, args.kind));
+      });
+    });
+
+    done();
+  }
+
   // function _addAlias(seneca, from, to) {
   //   if (to === from) {
   //     console.log('Ignoring alias ' + from + ' => ' + to);
--- test/unit.js
+++ test/unit.js
@@ -1,34 +1,63 @@
 const assert = require('chai').assert;
 const simple = require('simple-mock');
+const Common = require('seneca/lib/common');
 
 const Plugin = require('../ember-rest-adapter');
 
+function SenecaMock() {
+  this.util = {
+    deepextend: Common.deepextend
+  };
+  this.ready = function () {
+  }
+}
+
 describe('Unit | Plugin creation', function () {
   var plugin,
-    seneca = {},
+    seneca = new SenecaMock(),
     add;
-
-  function makeInstance(options) {
-    return Plugin.call(seneca, options || {});
-  }
 
   before(function (done) {
     add = simple.mock(seneca, 'add');
     done();
   });
 
+  beforeEach(function (done) {
+    plugin = Plugin.call(seneca);
+    done();
+  });
+
   it('should set plugin name', function (done) {
-    const plugin = makeInstance();
     assert.equal(plugin.plugin, 'ember-rest-adapter');
     done();
   });
 
-  it('should subscribe pattern init:ember-rest-adapter', function (done) {
-    makeInstance();
+  it('should subscribe to pattern init:ember-rest-adapter', function (done) {
     assert.ok(add.called);
     const args = add.calls[0].args;
     assert.deepEqual(args[0], {init: 'ember-rest-adapter'});
     assert.isFunction(args[1]);
     done();
+  });
+});
+
+describe('Unit | Plugin initialization', function () {
+  var plugin,
+    seneca = new SenecaMock(),
+    init;
+
+  before(function (done) {
+    add = simple.mock(seneca, 'add');
+    done();
+  });
+
+  beforeEach(function (done) {
+    plugin = Plugin.call(seneca);
+    init = add.calls[0].args[1];
+    done();
+  });
+
+  it('should call callback', function (done) {
+    init.call(seneca, {}, done);
   });
 });
